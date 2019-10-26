eb373a50d526ccef288ba72f82a22cbba9193ff2
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2019-10-27 00:39:12 +0900

    Create 20191026_완주하지못한선수.py

---
+++ 20191026_완주하지못한선수.py
@@ -0,0 +1,13 @@
+#나의 풀이
+ def solution(participant,completion):
+	answer = ""
+	participant.sort()
+	completion.sort()
+	completion.insert(len(participant),"zzzzzzzzzzzzzzzzzzzzz")
+	for i in range(0,len(participant)):
+		if participant[i] != completion[i]:
+		  answer = participant[i]
+		  break
+	return answer.lower()
+	
+

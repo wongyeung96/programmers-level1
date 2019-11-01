1759395da9645b3d96a37e0e91661c5c679706f5
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2019-11-01 11:50:39 +0900

    Update 191101_같은숫자는싫어.py

--- 191101_같은숫자는싫어.py
+++ 191101_같은숫자는싫어.py
@@ -3,7 +3,7 @@ programmers algorism study
 def solution(arr):
     answer = []
     answer.append(arr[0])
-    for i in range(1,len(arr)-1):
+    for i in range(1,len(arr)):
         if arr[i] != arr[i-1]:
             answer.append(arr[i])
     return answer

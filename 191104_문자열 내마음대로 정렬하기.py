b60c79bcb0457c34871ce645c88f73c34a91acc7
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2019-11-05 11:19:09 +0900

    Update 191104_문자열 내마음대로 정렬하기.py

--- 191104_문자열 내마음대로 정렬하기.py
+++ 191104_문자열 내마음대로 정렬하기.py
@@ -1,12 +1,19 @@
 # programmers
 programmers algorism study
 def solution(strings,n):
+    #1) 1차 출력물 plus와 2차 출력물 answer로 나눔
     plus =[]
     answer = []
+    
+    #2) n번째 문자를 i의 문자 맨앞에 붙여주고 오름차순 정렬.
     for i in strings:
         a = i[n] + i
         plus.append(a)
     plus.sort()
+    
+    #3) 오름차순 정렬 한 plus에서 필요없는 부분인 0부분을 뺀 나머지 값만 answer에 저장. 
     for j in plus:
         answer.append(j[1:])
+    
+    #4) 
     return answer

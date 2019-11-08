e8ccba7ffd6605e0c30b778b2d1e19c63822b853
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2019-11-08 09:51:29 +0900

    Create 191108_서울에서 김서방 찾기.py

---
+++ 191108_서울에서 김서방 찾기.py
@@ -0,0 +1,8 @@
+# programmers
+programmers algorism study
+def solution(seoul):
+    print("김서방은 {0}에 있다".format(seoul.index("Kim")))
+
+#리스트이름.index('a')는 리스트 안의 'a'의 위치를 반환해준다.
+#"안녕 나는 {}이야.".format('sam')을 실행하면 {}위치에 'sam'을
+# 넣어준다.

add6f4bdd07372e3f0d121a40dce15f531fea283
wongyeung96 <57033367+wongyeung96@users.noreply.github.com>
2020-01-08 15:37:47 +0900

    Create 200108_짝수와 홀수.py

---
+++ 200108_짝수와 홀수.py
@@ -0,0 +1,11 @@
+# programmers
+programmers algorism study
+
+def solution(num):
+    #만약 nun을 2로 나눈 나머지가 0이면 "Even"을 s에 저장
+    if num%2==0 :
+        s = "Even"
+    #만약 nun을 2로 나눈 나머지가 0이 아니면 "Odd"을 s에 저장
+    else :
+        s = "Odd"
+    return s
